b37997a671c16066d0e7eaebc79b86c3e984e62f
bmalvo <boydmalvo@gmail.com>
2024-05-09 12:24:33 +0200

    building main structure

---
+++ src/Stopwatch.tsx
@@ -0,0 +1,85 @@
+import { useState, useEffect } from "react";
+
+const lapsFormated: Array<string> = [];
+const laps:Array<number> = []
+
+
+export const Stopwatch = () => {
+  const [time, setTime] = useState(0);
+  const [currentLap, setCurrentLap] = useState(0);
+  const [isRunning, setIsRunning] = useState(false);
+  
+  const handleStart = () => {
+    setIsRunning(true);
+    };
+
+  const handleStop = () => {
+    setIsRunning(false);
+  };
+
+  const handleReset = () => {
+    setTime(0);
+    setCurrentLap(0);
+  };
+  
+  const handleLap = () => {
+    lapsFormated.push(formatTime(currentLap))
+    laps.push(currentLap);
+    setCurrentLap(0);
+    lapsFormated.sort();
+    console.log(lapsFormated)
+    console.log(laps)
+  }
+
+  useEffect(() => {
+    if (isRunning) {
+      const interval = setInterval(() => {
+        setTime((prevTime) => prevTime + 0.01),
+        setCurrentLap(prevTime => prevTime + 0.01);
+      }, 10);
+      
+      return () => clearInterval(interval);
+    }
+  }, [isRunning]);
+  
+  const formatTime = (time: number) => {
+    const minutes = Math.floor(time / 60);
+    const seconds = Math.floor(time % 60);
+    const milliseconds = Math.floor((time % 1) * 100);
+    
+    return `${minutes}:${seconds}:${milliseconds}`;
+  };
+  
+  const avrLapsTime = () => {
+    const average = laps.reduce((acc, curr) => acc + curr, 0) / laps.length;
+    return formatTime(average);
+  }
+  // Time Laps
+
+  const timeLaps = () => {
+
+
+    return <div>
+      <p>Time: {laps? formatTime(time): ''}</p>
+      <p>Average time: { avrLapsTime() }</p>
+      <p>Fastest lap: { lapsFormated[0] }</p>
+      <p>Slowest lap: { lapsFormated[lapsFormated.length-1] }</p>
+      <p>Laps number: { lapsFormated.length }</p>
+    </div>
+  }
+
+  return (<>
+      <div>
+        <p>{formatTime(time)}</p>
+        <p>{formatTime(currentLap)}</p>
+        <button onClick={handleStart}>Start</button>
+        <button onClick={handleStop}>Stop</button>
+        <button onClick={handleReset}>Reset</button>
+        <button onClick={handleLap}>Lap</button>
+      </div>
+    <div>
+      {timeLaps()}
+      </div>
+    </>
+    )
+  }

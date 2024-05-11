8685fd15034cda0e3e42f78491f79e0d77ddb537
bmalvo <boydmalvo@gmail.com>
2024-05-11 17:54:03 +0200

    add Button.tsx

---
+++ src/Button.tsx
@@ -0,0 +1,12 @@
+import { MouseEventHandler } from "react";
+
+type ButtonProps = {
+    name: string;
+    method: MouseEventHandler<HTMLButtonElement>;
+}
+
+export const Button = ({ name, method }: ButtonProps) => {
+
+  
+    return <button onClick={method}>{ name }</button>
+}

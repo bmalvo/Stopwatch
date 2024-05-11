723576792c94ac783a9075f40502aed0190a60b2
bmalvo <boydmalvo@gmail.com>
2024-05-11 19:04:47 +0200

    add FormatTime.tsx

---
+++ src/FormatTime.tsx
@@ -0,0 +1,12 @@
+type FormatTimeProps = {
+    time: number;
+}
+
+export const FormatTime = ({ time }:FormatTimeProps ) => {
+    const minutes = Math.floor(time / 60);
+    const seconds = Math.floor(time % 60);
+    const milliseconds = Math.floor((time % 1) * 100);
+    
+    return `${minutes}:${seconds}:${milliseconds}`;
+};
+  

e38ae7b6cab6d5698a98e49241ad769d0a594f0d
bmalvo <boydmalvo@gmail.com>
2024-05-11 23:28:30 +0200

    add TimeLaps.tsx

---
+++ src/TimeLaps.tsx
@@ -0,0 +1,24 @@
+import { FormatTime } from "./FormatTime";
+
+type TimeLapsProps = {
+    arrayString: Array<string>;
+    arrayNumber: Array<number>;
+    time: number;
+}
+
+export const TimeLaps = ({ arrayString, arrayNumber, time }: TimeLapsProps) => {
+     
+    const avrLapsTime = () => {
+        const averageTime = arrayNumber.reduce((acc, curr) => acc + curr, 0) / arrayNumber.length;
+        return <FormatTime time={averageTime} />;
+    }
+
+
+    return <div>
+        <p>Time: {arrayNumber ? <FormatTime time={time} /> : ''}</p>
+        <p>Average time: {avrLapsTime()}</p>
+        <p>Fastest lap: {arrayString[0]}</p>
+        <p>Slowest lap: {arrayString[arrayString.length - 1]}</p>
+        <p>Laps number: {arrayString.length}</p>
+    </div>
+};
